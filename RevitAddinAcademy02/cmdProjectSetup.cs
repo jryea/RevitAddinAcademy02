9492380ea0d4aad243a8881f911a9c2df131982d
Jon ryea <jryea80@gmail.com>
2022-07-15 14:23:12 -0600

    Week 3 challenge commit

--- RevitAddinAcademy02/cmdProjectSetup.cs
+++ RevitAddinAcademy02/cmdProjectSetup.cs
@@ -50,76 +50,119 @@ namespace RevitAddinAcademy02
             Excel.Range excelRng2 = wkSheet2.UsedRange;
             int rowCount1 = excelRng1.Rows.Count;
             int rowCount2 = excelRng2.Rows.Count;
+
+
+            //Collecting Titleblocks
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_TitleBlocks);                              //Collects the titleblocks
+            collector.WhereElementIsElementType();                                              //Method that sets the element collection to Type
             
-            
+            //Collecting views
+            FilteredElementCollector viewCollector = new FilteredElementCollector(doc);
+            viewCollector.OfClass(typeof(ViewFamilyType));                                                            
+
+            ViewFamilyType floorVFT = null;
+            ViewFamilyType rcpVFT = null;
+
+            foreach (ViewFamilyType type in viewCollector)
+            {
+                if (type.ViewFamily == ViewFamily.FloorPlan)
+                {
+                    floorVFT = type;
+                }
+                else if (type.ViewFamily == ViewFamily.CeilingPlan)
+                {
+                    rcpVFT = type;
+                }
+            }
 
             using (Transaction t = new Transaction(doc))
             {
                 t.Start("Transaction");
 
-                
-               /* //Creating Views
-                for (int i = 2; i<=rowCount1; i++)
+               
+                // Creating Levels and Views
+
+                for (int i = 2; i <= rowCount1/2; i++)
                 {
-                    Excel.Range cellViewName = excelRng1.Rows[i,3];
-                    string viewName = cellViewName.Value.ToString();
-
-                    FilteredElementCollector viewCollector = new FilteredElementCollector(doc);
-                    viewCollector.OfClass(typeof(ViewFamilyType));                                  //Collects Views                            
-
-                    ViewFamilyType floorVFT = null;
-                    ViewFamilyType rcpVFT = null;
-
-                    foreach(ViewFamilyType type in viewCollector)
-                    {
-                        if(type.ViewFamily == ViewFamily.FloorPlan)
-                        {
-                            floorVFT = type;
-                        }
-                        else if(type.ViewFamily == ViewFamily.CeilingPlan)
-                        {
-                            rcpVFT = type;
-                        }
-                    }
-
-                   // ViewPlan viewFP = ViewPlan.Create(doc,floorVFT.Id, newLevel.Id);
-
-                }
-
-                */
-                // Creating Levels
-
-                for (int i = 2; i <= rowCount1; i++)
-                {
-                    Excel.Range cellLevelName = wkSheet1.Cells[i, 1];       //Sets the cell to a variable
+                    Excel.Range cellLevelName = wkSheet1.Cells[i, 1];                    //Sets the cell to a variable
                     Excel.Range cellElevation = wkSheet1.Cells[i, 2];
-
-                    string levelName = cellLevelName.Value.ToString();      //Returns the value of a cell
+      
+                    string levelName = cellLevelName.Value.ToString();                   //Returns the value of a cell
                     double elevation = cellElevation.Value;
                 
-                    Level level = Level.Create(doc,elevation);              //Creating the level
-                    level.Name = levelName;                                 //Setting the name of the level
+                    Level level = Level.Create(doc,elevation);                           //Creating the level
+                    level.Name = levelName;                                              //Setting the name of the level
+                    ViewPlan curPlan = ViewPlan.Create(doc, floorVFT.Id, level.Id);     //Creating a floor plan of the current level
+                    ViewPlan curRCP = ViewPlan.Create(doc, rcpVFT.Id, level.Id);        //Creating a RCP of the current level
+                    curRCP.Name = curRCP.Name + " RCP";
                 }
 
                 // Creating Sheets
 
+                                                              
+                FilteredElementCollector collector3 = new FilteredElementCollector(doc);  //Collecting Views
+                collector3.OfClass(typeof(View));
+
                 for (int i = 2; i <= rowCount2; i++)
                 {
 
-                    FilteredElementCollector collector = new FilteredElementCollector(doc);
-                    collector.OfCategory(BuiltInCategory.OST_TitleBlocks);                              //Collects the titleblocks
-                    collector.WhereElementIsElementType();                                              //Method that sets the element collection to Type
-
+                   
                     Excel.Range cellSheetNumber = wkSheet2.Cells[i, 1];
                     Excel.Range cellSheetName = wkSheet2.Cells[i,2];
+                    Excel.Range cellView = wkSheet2.Cells[i, 3];
+                    Excel.Range cellDrawnBy = wkSheet2.Cells[i, 4];
+                    Excel.Range cellCheckedBy = wkSheet2.Cells[i, 5];
 
                     string sheetNumber = cellSheetNumber.Value.ToString();
-                    string sheetName = cellSheetName.Value.ToString(); 
-                      
+                    string sheetName = cellSheetName.Value.ToString();
+                    string viewName = cellView.Value.ToString();
+                    string sheetDrawnBy = cellDrawnBy.Value.ToString();
+                    string sheetCheckedBy = cellCheckedBy.Value.ToString();
 
                     ViewSheet sheet = ViewSheet.Create(doc, collector.FirstElementId());                //Creates a new sheet
                     sheet.SheetNumber = sheetNumber;
                     sheet.Name = sheetName;
+
+                    foreach(Parameter curParam in sheet.Parameters)                                     //Cycles through all parameters of the current sheet
+                    {
+                        if(curParam.Definition.Name == "Drawn By")
+                        {
+                            curParam.Set(sheetDrawnBy);
+                        }
+                        if(curParam.Definition.Name == "Checked By")
+                        {
+                            curParam.Set(sheetCheckedBy);
+                        }
+                    }
+
+                    //Setting Views on Sheets
+
+                    View existingView = null;
+                    foreach(View curView in collector3)
+                    {
+                        if(curView.Name == viewName)
+                        {
+                            existingView = curView;
+                            TaskDialog.Show("existingView.Name", existingView.Name);
+                            TaskDialog.Show("CurView.Name", curView.Name);
+                            TaskDialog.Show("viewName", viewName);
+                        }
+                        
+                    }
+
+                    if (existingView != null)
+                    {
+
+                        Viewport newVP = Viewport.Create(doc, sheet.Id, existingView.Id, new XYZ(0, 0, 0));
+                    }
+                    else
+                    {
+                        TaskDialog.Show("Error", "Could not find view");
+                    }
+                    
+
+
                 }
 
                 t.Commit();
